d902820dc51369ff33ed29e8a23ca8f517dbbc3b
RokhayaNC <rokhaya0805@gmail.com>
2025-04-23 23:29:29 +0000

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ const connectDB = require("./config/db.js");
 const authRoutes = require('./routes/authRoutes'); 
 const donRoutes = require('./routes/donRoutes');
 
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -24,17 +22,12 @@ app.use(cors({
 }));
 
 
-  
-
-app.use(express.json());
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/dons', donRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.options('*', cors());
+//app.options('*', cors());
 
 
 
