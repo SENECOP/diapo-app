88d2ce6db883e8778af7006683a6b77c6281ec9f
RokhayaNC <rokhaya0805@gmail.com>
2025-05-11 22:18:40 +0000

    Fixes reservation and button state

--- frontend/src/components/CardDon.js
+++ frontend/src/components/CardDon.js
@@ -67,7 +67,6 @@ const CardDon = ({ don, onReservationSuccess }) => {
       navigate("/message", {
         state: { showReservationAlert: true },
       });
-      localStorage.setItem("AlerteReservation", "true");
 
     } catch (error) {
       console.error("Erreur lors de la réservation :", error);
